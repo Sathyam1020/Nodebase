6fbb87d72230604ba85a67a114e53c444fc9c76d
Sathyam Sahu <sathyamrock55@gmail.com>
2025-10-19 23:48:11 +0530

    Auth setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
-'use client'
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
-const page = () => {
-
-    const trpc = useTRPC();  
-    const {data: users} = useQuery(trpc.getUsers.queryOptions()); 
-
+const page = async() => {
+  // const { data } = authClient.useSession();
+  await requireAuth(); 
+  const data = await caller.getUsers(); 
   return (
-    <div>
-      {JSON.stringify(users)}
+    <div className="">
+      {JSON.stringify(data)}
+      
     </div>
   )
 }
 
-export default page;  
- 
+export default page;
